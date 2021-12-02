7d5849f42dbe04e6c456a45d6e0ba50ea3e2c29f
Your Name <you@example.com>
2021-12-02 16:34:08 +0000

    commit AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,53 +1,54 @@
-import React,{ useState } from "react";
- import { usePagination } from "../components/utils/pagination.jsx";
-
+import React, { useState } from "react";
+import { usePagination } from "../components/utils/pagination.jsx";
 
 export const AppContext = React.createContext();
 
-
 export default function AppProvider({ children }) {
-    const [user,setUser] = useState({})
-    const [points, setPoints] = useState(0)
-    const [products, setProducts] = useState([])
-    const [reedemStatus, setReedemStatus] = useState({})
-    const [history, setHistory] = useState([])
+  const [user, setUser] = useState({});
+  const [points, setPoints] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [reedemStatus, setReedemStatus] = useState({});
+  const [history, setHistory] = useState([]);
 
+  const paginationList = usePagination(products, 16);
+  const paginationHistoryList = usePagination(history, 16);
 
-    const paginationList = usePagination(products, 16)
-    const paginationHistoryList = usePagination(history, 16)
+  const totalProducts = products.length;
+  const totalHistory = history.length;
 
-    const totalProducts = products.length
-    const totalHistory = history.length
+  const handlerAddPoint = (value) => {
+    const newUser = { ...user };
+    newUser.points = user.points + value;
+    setUser(newUser);
+  };
 
-    const handlerAddPoint =(value)=>{
-        const newUser = {...user}
-        newUser.points = user.points + value
-        setUser(newUser)
-      }
-    
-      const handlerSubtractPoint =(points)=>{
-        const newUser = {...user}
-        newUser.points = user.points - points
-        setUser(newUser)
-      }
-    return(
-        <AppContext.Provider value={{  user,
-      setUser,
-            handlerAddPoint, 
-            handlerSubtractPoint, 
-            points,
-            setPoints,  
-            products, 
-            setProducts, 
-            totalProducts,
-            paginationList,
-            reedemStatus, 
-            setReedemStatus,
-            history,
-             setHistory, 
-             paginationHistoryList,
-             totalHistory}}>
-             {children}
-        </AppContext.Provider>
-    );
+  const handlerSubtractPoint = (points) => {
+    const newUser = { ...user };
+    newUser.points = user.points - points;
+    setUser(newUser);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        handlerAddPoint,
+        handlerSubtractPoint,
+        points,
+        setPoints,
+        products,
+        setProducts,
+        totalProducts,
+        paginationList,
+        reedemStatus,
+        setReedemStatus,
+        history,
+        setHistory,
+        paginationHistoryList,
+        totalHistory,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
